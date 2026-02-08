b2a7c940d572a46ef09666b747fe187560a0628d
alderlima <aldemice@gmail.com>
2026-02-08 04:13:03 -0300

    Correções para build no GitHub Actions

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class RastreadorApp extends StatelessWidget {
             brightness: Brightness.dark,
           ),
           useMaterial3: true,
-          cardTheme: CardTheme(
+          CardThemeData: CardThemeData(
             elevation: 8,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
